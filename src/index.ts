1506080e9a452e6e39fecf29bb2ce5b5d9e13e92
Zvezke <superur@gmail.com>
2024-02-12 15:06:52 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.215.0/http/server.ts";
 
 let port = parseInt(Deno.env.get("PORT") ?? "8000");
 const s = serve({ port });
